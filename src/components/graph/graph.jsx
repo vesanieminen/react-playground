adb4111eb62010b5d9bb17a8f176da961dfb45c2
Vesa Nieminen <vesa@vaadin.com>
2016-09-23 16:54:32 +0300

    Added graph data.

---
+++ src/components/graph/graph.jsx
@@ -0,0 +1,18 @@
+'use strict';
+
+var React = require('react');
+var fs = require('fs');
+
+var Graph = React.createClass({
+
+render: function() {
+    return (
+        <div>
+            <h1>Yo Graphian!</h1>
+        </div>
+    );
+}
+
+});
+
+module.exports = Graph;
